172343ef78854a207908b9c43d83d96b6355bba7
Anjali Gogu <anjali.gogu@everest.engineering>
2024-12-15 21:28:20 +0530

    Add CORS, body-parser, and user routes to server setup

--- server.ts
+++ server.ts
@@ -1,9 +1,21 @@
 import express from "express";
 import { connectToDatabase } from "./src/DatabaseConnection";
+import userRouter from "./src/routes/UserRoutes";
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT || 3000;
-connectToDatabase();
+try {
+  connectToDatabase();
+} catch (error) {
+  console.error("Error connecting to the database", error);
+}
+app.use("/api/users", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
